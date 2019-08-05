4940f53a5c7f4f3cfa1a43bee9e2b9d662d01dbc
neha08585 <neha_08585@yahoo.co.in>
2019-08-05 18:36:14 -0400

    commiting all changes

--- gameplay/admin.py
+++ gameplay/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from .models import Game
-from .models import Move
 
-
-@admin.register(Game)
-class GameAdmin(admin.ModelAdmin):
-    list_display = ('id','first_player', 'second_player', 'status')
-    list_editable = ('status',)
+from .models import Game, Move
 
 
 admin.site.register(Move)
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_player', 'second_player', 'status')
+    list_editable = ('status',)
--- player/forms.py
+++ player/forms.py
@@ -2,7 +2,8 @@ from django.forms import ModelForm
 
 from .models import Invitation
 
+
 class InvitationForm(ModelForm):
     class Meta:
         model = Invitation
-        exclude = ('from_user','timestamp')
+        exclude = ('from_user', 'timestamp')
